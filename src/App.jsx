215c01d0a1f7f649a1dbfa1594f3e15d60d7d3bc
TanPhat310309 <duckydaily044@gmail.com>
2026-04-05 10:32:32 +0700

    Add multiple files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+function App() {
+  const location = useLocation();
+  return (
+    <>
+ <Header />
+      
+    
+    </>
+  );
+}
+
+export default App;
