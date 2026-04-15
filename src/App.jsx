028f1ec7b373efa2d4ede4a668f115d8bc59b153
TanPhat310309 <duckydaily044@gmail.com>
2026-04-15 15:46:59 +0700

    Import DetailProduct component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import HeaderGPT_4 from './components/Header/HeaderGPT_4';
+import DetailProduct from './components/Body/DetailProduct';
 function App() {
   const location = useLocation();
   return (
     <>
  <HeaderGPT_4 />
-
+<DetailProduct />
   <Footer />
   
     </>
