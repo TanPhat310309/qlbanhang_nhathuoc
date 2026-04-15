19c8becacb83c6d9851bfb70f4d570bbe0347a76
TanPhat310309 <duckydaily044@gmail.com>
2026-04-15 13:03:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,16 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import HeaderGPT_4 from './components/Header/HeaderGPT_4';
-import products from './data.json';
-const products = [
-  { "id": 1, "name": "Amoxicillin 500mg", "categoryid": 1, "price": 50000, "image": "src/img/amoxicillin500mg.png" },
-  { "id": 2, "name": "Augmentin 1g", "categoryid": 1, "price": 180000, "image": "src/img/augmentin1g.png" },
-];
-
 function App() {
   const location = useLocation();
-
   return (
     <>
-      <HeaderGPT_4 />
-      
-      {}
-      <main>
-        {products.map((item) => (
-          <img key={item.id} src={item.image} alt="" />
-        ))}
-      </main>
+ <HeaderGPT_4 />
+  
 
-      <Footer />
+
+  <Footer />
+  
     </>
   );
 }
