f168d5ee7338d68445c6e7aa8212ce34e6a049a0
TanPhat310309 <duckydaily044@gmail.com>
2026-04-15 12:55:59 +0700

    Display product images in the main content
    Render product images in the main section of the app.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import HeaderGPT_4 from './components/Header/HeaderGPT_4';
+
+const products = [
+  { "id": 1, "name": "Amoxicillin 500mg", "categoryid": 1, "price": 50000, "image": "src/img/amoxicillin500mg.png" },
+  { "id": 2, "name": "Augmentin 1g", "categoryid": 1, "price": 180000, "image": "src/img/augmentin1g.png" },
+];
+
 function App() {
   const location = useLocation();
+
   return (
     <>
- <HeaderGPT_4 />
-  
-    <div className='container'></div>
+      <HeaderGPT_4 />
+      
+      {}
+      <main>
+        {products.map((item) => (
+          <img key={item.id} src={item.image} alt="" />
+        ))}
+      </main>
 
-  <Footer />
-  
+      <Footer />
     </>
   );
 }
