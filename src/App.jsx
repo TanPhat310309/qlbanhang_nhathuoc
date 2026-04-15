0c2c355dc0ed272f85e4ce39d6f8f1404a64d238
TanPhat310309 <duckydaily044@gmail.com>
2026-04-15 15:51:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Footer from './components/Footer/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import HeaderGPT_4 from './components/Header/HeaderGPT_4';
 import DetailProduct from './components/Body/DetailProduct';
+import ProductList from './components/Body/ProductList';
 function App() {
   const location = useLocation();
   return (
     <>
  <HeaderGPT_4 />
+        <ProductList />
 <DetailProduct />
   <Footer />
   
